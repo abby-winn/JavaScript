19e979e40e924ba10f1ad434a0195228c3cbbe04
Abby <abigailjwinn@gmail.com>
2023-06-01 10:26:37 -0500

    fixed

--- longestNameFinder.js
+++ longestNameFinder.js
@@ -2,13 +2,14 @@ let name1 = prompt("Enter your last name");
 let name2 = prompt("Enter another last name");
 let name3 = prompt("Enter another last name");
 const names = [name1, name2, name3];
-const longestName = function calcLongestName(names) {
-    highest = 0;
+function calcLongestName(names) {
+    let highest = 0;
     for (let i = 0; i<names.length; i++) {
         before = names[i].length;
         if (before > highest) {
-            highest = before;
+            highest = names[i];
         }
     }
+    console.log(highest);
 }
-print(longestName)
+calcLongestName(names);
