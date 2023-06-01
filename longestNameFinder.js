7fdb08f4ba96d279fc0d8b6c07d1a6240380f998
Abby <abigailjwinn@gmail.com>
2023-06-01 10:29:12 -0500

    changed to arrow notation

--- longestNameFinder.js
+++ longestNameFinder.js
@@ -2,7 +2,7 @@ let name1 = prompt("Enter your last name");
 let name2 = prompt("Enter another last name");
 let name3 = prompt("Enter another last name");
 const names = [name1, name2, name3];
-function calcLongestName(names) {
+const calcLongestName = (names) => {
     let highest = 0;
     for (let i = 0; i<names.length; i++) {
         before = names[i].length;
